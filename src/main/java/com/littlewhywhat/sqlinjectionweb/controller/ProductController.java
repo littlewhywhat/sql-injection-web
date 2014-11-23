ef0f9ffd67bb71857d3072ce8578db731395caed
roman <littlewhywhat@gmail.com>
2014-11-23 19:30:34 +0300

    added bean ProductRepository using JDBC 	modified:   pom.xml: added jdbc and hsqldb dependencies 	new file:   src/main/java/application.properties: added 	modified:   src/main/java/../controller/ProductController.java: added Autowired products 	new file:   src/main/java/../repository/ProductRepository.java: added

--- src/main/java/com/littlewhywhat/sqlinjectionweb/controller/ProductController.java
+++ src/main/java/com/littlewhywhat/sqlinjectionweb/controller/ProductController.java
@@ -1,12 +1,14 @@
 package sqlinjectionweb.controller;
 
 import sqlinjectionweb.repository.Product;
+import sqlinjectionweb.repository.ProductRepository;
 
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.List;
 import java.util.ArrayList;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,12 +17,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class ProductController {
 
-    private final List<Product> list = new ArrayList<Product>();
     private final AtomicLong counter = new AtomicLong();
+    private final List<Product> list = new ArrayList<Product>();
+
+    @Autowired
+    private ProductRepository products;
 
     @RequestMapping(value = "/", method=RequestMethod.GET)
     public @ResponseBody List<Product> getProductList() {
-        return list;
+        return products.findAll();
     }
 
     @RequestMapping(value="/add", 
---
+++ src/main/java/com/littlewhywhat/sqlinjectionweb/repository/ProductRepository.java
@@ -0,0 +1,43 @@
+package sqlinjectionweb.repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+@Component
+public class ProductRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public ProductRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+        clean();
+        init();
+    }
+
+    private void clean() {
+        jdbcTemplate.execute("drop table products if exists");
+    }
+
+    private void init() {
+        jdbcTemplate.execute("create table products(" +
+                "id int, name varchar(255))");
+        jdbcTemplate.execute("INSERT INTO products(id, name) " + 
+                     "values(0 , 'hello')");  
+    }
+
+    public List<Product> findAll() {
+        return jdbcTemplate.query("select * from products", new RowMapper<Product>() {
+                    @Override
+                    public Product mapRow(ResultSet rs, int rowNum) throws SQLException {
+                        return new Product(rs.getLong("id"), rs.getString("name"));
+                    }
+                });
+    }
+}
