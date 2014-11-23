155dd9a222410bb9b2337f50fb738a6e62e98881
roman <littlewhywhat@gmail.com>
2014-11-23 18:57:29 +0300

    ProductController.java: refactored, diversed addition

--- src/main/java/com/littlewhywhat/sqlinjectionweb/controller/ProductController.java
+++ src/main/java/com/littlewhywhat/sqlinjectionweb/controller/ProductController.java
@@ -6,22 +6,28 @@ import java.util.concurrent.atomic.AtomicLong;
 import java.util.List;
 import java.util.ArrayList;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class ProductController {
 
     private final List<Product> list = new ArrayList<Product>();
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/")
-    public List<Product> product(@RequestParam(value="name", defaultValue="") String name) {
-        if (name.length() != 0) {
-            long id = counter.incrementAndGet();
-            list.add(new Product(id, name));
-        }
+    @RequestMapping(value = "/", method=RequestMethod.GET)
+    public @ResponseBody List<Product> getProductList() {
         return list;
     }
+
+    @RequestMapping(value="/add", 
+                    method=RequestMethod.GET)
+    public @ResponseBody boolean addProduct(
+        @RequestParam("name") String name) {
+        long id = counter.incrementAndGet();
+        return list.add(new Product(id, name));
+    } 
 }
